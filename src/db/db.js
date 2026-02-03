bc61ece22699ee7b3b3d7456c4daa35a0d9b5c09
Khangdang1690 <khangdang0199@gmail.com>
2026-02-02 21:34:49 -0500

    Implement database schema

---
+++ src/db/db.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema.js";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in .env file');
+}
+
+export const db = drizzle(process.env.DATABASE_URL, { schema });
